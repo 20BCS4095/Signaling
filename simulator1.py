ed6aa9ad869dda7286f3819aabffed0d9bc2717e
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-03-21 10:01:16 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -1,25 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-json_data_with_headers = {}
+stored_binary_data = b''
 
 @app.route('/post_json', methods=['POST'])
 def post_json():
-    if not request.is_json:
-       return jsonify({'error': 'Request must contain JSON data'}), 400
-    json_data = request.get_json()
-    headers = dict(request.headers)
-    global json_data_with_headers
-    json_data_with_headers['data'] = json_data
-    json_data_with_headers['headers'] = headers
-
-    return jsonify({'message': 'JSON data and headers stored successfully'}), 200
+    global stored_binary_data  
+    if request.data:
+        stored_binary_data = request.data
+        return 'Binary data uploaded successfully', 200
+    else:
+        return 'No binary data received', 400
 
 @app.route('/view', methods=['GET'])
 def get_json():
-    global json_data_with_headers
-    return render_template('index.html', data=json_data_with_headers)
+    global stored_binary_data
+    if stored_binary_data:
+        return render_template('index.html', data=stored_binary_data)
+    else:
+        return 'No binary data stored', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
