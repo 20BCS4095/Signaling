117ad2f5b3003decfc18fa8a1e1c23ad029d8aff
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-03-25 16:22:16 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -1,9 +1,68 @@
 from flask import Flask, request, render_template
+import datetime
 
 app = Flask(__name__)
 
 stored_binary_data = b''
-
+def conversion():
+    data={}
+    decimal_values = list(stored_binary_data)
+    version_found = False
+    command_found = False
+    collection_id_found = False
+    nonce_found = False
+    timestamp_found = False
+    encrypted_block_found = False
+    version = ""
+    command = ""
+    collection_id = ""
+    nonce = ""
+    timestamp = ""
+    encrypted_block = ""
+    other = ""
+    i = 0
+    while i < len(decimal_values):
+        if decimal_values[i] == 9 and not version_found:
+            version = decimal_values[i+1]
+            data["9"]=version
+            i=i+1
+            version_found = True
+        elif decimal_values[i] == 25 and not command_found:
+            command = decimal_values[i+1]
+            data["25"]=command
+            i=i+1
+            command_found = True
+        elif decimal_values[i] == 20 and not collection_id_found:
+            ascii_list = [decimal_values[i+1], decimal_values[i+2], decimal_values[i+3], decimal_values[i+4]]
+            collection_id = ''.join(chr(code) for code in ascii_list)
+            data["20"]=collection_id
+            i=i+4
+            collection_id_found = True
+        elif decimal_values[i] == 156 and not nonce_found:
+            int_array = [decimal_values[i+1], decimal_values[i+2], decimal_values[i+3], decimal_values[i+4]]
+            nonce = ' '.join(map(str, int_array))
+            data["156"]=nonce
+            i=i+4
+            nonce_found = True
+        elif decimal_values[i] == 68 and not timestamp_found:
+            int_array = [decimal_values[i+1], decimal_values[i+2], decimal_values[i+3], decimal_values[i+4]]
+            hex_timestamp = ''.join(map(str, int_array))
+            time = int(hex_timestamp, 16)
+            datetime_obj = datetime.datetime.fromtimestamp(time)
+            timestamp = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+            data["68"]=timestamp
+            i=i+4
+            timestamp_found = True
+        elif decimal_values[i] == 134 and not encrypted_block_found:
+            encrypted_block = decimal_values[i+1]
+            data["134"]=encrypted_block
+            i=i+1
+            encrypted_block_found = True
+        else:
+            other += str(decimal_values[i]) + " "
+        i += 1
+    data['other']=other
+    return data
 @app.route('/',methods=['GET'])
 def home_page():
     return render_template('home.html')
@@ -33,7 +92,7 @@ def packet_decoder():
     return render_template('PacketDecoder.html')
 
 @app.route('/post_json', methods=['POST','GET'])
-def post_json():
+def post_data():
     global stored_binary_data 
     if request.method == 'POST':
         if request.data:
@@ -53,8 +112,9 @@ def get_json():
     if stored_binary_data:
         decimal_values = [char for char in stored_binary_data]
         decimal_string = ' '.join(map(str, decimal_values))
-        data_size = len(stored_binary_data)
-        return render_template('index.html', data=decimal_string, binary_data=stored_binary_data, data_size=data_size)
+        data_size = len(decimal_string)
+        data=conversion()
+        return render_template('index.html', data=decimal_string, binary_data=stored_binary_data, data_size=data_size, data1=data)
     else:
         return 'No binary data stored', 404
 
