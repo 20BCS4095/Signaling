5a2724f1f84d0f7355d15a24f96fb8abe3a1a380
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-03-26 15:05:49 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -137,7 +137,7 @@ def post_json():
             return 'No data is received', 400
     elif request.method == 'GET':
         if stored_binary_data:
-            return 'Success', 200
+            return stored_binary_data, 200
         else:
             return 'No information available',400
 
