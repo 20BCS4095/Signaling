17ea6ac7ae3943520d031b81e84f2994b499173a
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-04-29 18:22:18 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -512,7 +512,7 @@ class SignalingData:
         return Values
 
     def response_packet():
-        global signaling_ack_by_server
+        global signaling_ack_by_server=0
         index=0
         decimal_values=[]
         encrypted_values=[]
