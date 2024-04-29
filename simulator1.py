25a70e6ce99b15e709b456f7c54d5f3d541a0090
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-04-29 11:36:31 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -641,11 +641,7 @@ def view_metrics():
     status_thread = threading.Thread(target=check_printer_status)
     status_thread.daemon = True
     status_thread.start()
-    epoch_time=last_request_time
-    normal_time = datetime.datetime.utcfromtimestamp(epoch_time)
-    m=normal_time.strftime('%Y-%m-%d %H:%M:%S')
-    print(m)
-    return render_template('ViewMetrics.html', printer_online=printer_status,printer_last_seen=m)
+    return render_template('ViewMetrics.html', printer_online=printer_status,printer_last_seen=last_request_time)
 
 @app.route('/duration_test',methods = ['GET'])
 def duration_test():
