f6d6f79c11ab10787f924a199a9ca5432d2326d5
SWETHA M <89173661+20BCS4095@users.noreply.github.com>
2024-05-09 15:07:07 +0530

    Update simulator1.py

--- simulator1.py
+++ simulator1.py
@@ -186,12 +186,13 @@ def repeat_function(duration):
     end_time =start_time +duration
     options=["start_tunnel_1","start_tunnel_2","start_tunnel_3","start_tunnel_4","echo","rtp_kick","fw_update","registration_subscription","cdm_pubsub_1","cdm_pubsub_2","cdm_pubsub_3","connectivity_configuration","device_configuration"]
     while time.time() < end_time:
+        print(time.time,end_time)
         num_keys = random.randint(1, min(2, len(options)))
         random_keys = random.sample(options, num_keys)
         for key in random_keys:
-            set_signaling_values[key] = 1
-            set_count += 1
-            logger1.info(f'Selected signaling {key}')
+           set_signaling_values[key] = 1
+           set_count += 1
+           logger1.info(f'Selected signaling {key}')
     time.sleep(50)
     logger1.info('------------------Duration test completed-----------------------')
     logger1.info(f'Total no of bit set by server -> {set_count}')
